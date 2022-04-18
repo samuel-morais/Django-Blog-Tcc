084b3cd6c42b1523be00211942fee3d3688dfad8
Samuel Morais <samuelcarlosbsb@hotmail.com>
2022-04-18 15:44:15 -0300

    setando a rota de url cursos

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,8 +1,9 @@
-from urllib import request
+
 from django.urls import path
+
 from . import views
 
-urlpatterns = {
-
-    path('', views.index, name = 'index')
-}
+urlpatterns = [
+    path('', views.index, name='index'),
+    path ('curso', views.curso, name='curso'),  #Mapeando a rota de Curso #
+]
