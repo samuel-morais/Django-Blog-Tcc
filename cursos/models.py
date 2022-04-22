df85325b48f0bc9899d63890b13af99ba67ce413
Samuel Morais <samuelcarlosbsb@hotmail.com>
2022-04-22 16:29:00 -0300

    criando os models de curso, migrations

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
+# Create Modelo de Cursos
 
-# Create your models here.
+class Cursos(models.Model):
+    nome_curso = models.CharField(max_length=200)
+    ementa_do_curso = models.TextField()
+    informacoes = models.TextField()
+    carga_horaria = models.IntegerField()
+    categoria= models.CharField(max_length=100)
+    data_curso = models.DateTimeField(default=datetime.now, blank=True)
--- cursos/views.py
+++ cursos/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    cursos = {
+        1:'Python',
+        2:'PHP',
+        3:'Html'
+    }  
+
+    dados = {
+        'nome_dos_cursos' : cursos
+    }
+    return render(request, 'index.html',dados)
+
+
 
 def curso(request):
     return render(request,'curso.html')
+
