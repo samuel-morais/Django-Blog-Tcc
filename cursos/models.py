3805ff8d351e31a5e55ae399a900e17cd82553d2
Samuel Morais <samuelcarlosbsb@hotmail.com>
2022-04-26 00:13:14 -0300

    vinculando app de Pessoas em Cursos

--- cursos/models.py
+++ cursos/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 # Create Modelo de Cursos
 
 
 class Curso(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_curso = models.CharField(max_length=200)
     ementa_do_curso = models.TextField()
     informacoes = models.TextField()
