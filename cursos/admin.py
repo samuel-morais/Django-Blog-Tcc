b84b8e9a5992e66b481b248253ba863f821306ea
Samuel Morais <samuelcarlosbsb@hotmail.com>
2022-04-25 23:30:47 -0300

    Alterando lista Cursos do DjangoAdmin

--- cursos/admin.py
+++ cursos/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Curso
 
-admin.site.register(Curso)
+# Alterando a lista de Curso Admin
+class ListandoCursos(admin.ModelAdmin):
+    list_display = ('id','nome_curso','categoria','carga_horaria')
+    list_display_links = ('id','nome_curso')
+
+admin.site.register(Curso,ListandoCursos)
