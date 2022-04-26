6590e37cb03648d74c04dfabbba296328d4fb148
Samuel Morais <samuelcarlosbsb@hotmail.com>
2022-04-26 15:52:20 -0300

    validando campos do formulario de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == 'POST':
@@ -6,7 +7,21 @@ def cadastro(request):
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
-        print(nome,email,senha,senha2)
+        if not nome.strip():
+            print('O campo nome não pode ficar em Branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo email não pode ficar em Branco')
+            return redirect('cadastro')  
+        if senha != senha2:
+            print('As senhas não são iguais')     
+            return redirect('cadastro') 
+        if User.objects.filter(email=email).exists():
+            print ("Usuário Já Cadastrado")
+            return redirect('cadastro')   
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print("Usuário Cadastrado com Sucesso !")    
         return redirect('login')
     else:
         return render(request,'usuarios/cadastro.html')
