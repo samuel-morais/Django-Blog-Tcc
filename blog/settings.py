6ffae90a61a1775125dadb3b54a884da68ebded7
Samuel Morais <samuelcarlosbsb@hotmail.com>
2022-04-28 10:32:56 -0300

    deletando app de pessoas

--- blog/settings.py
+++ blog/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cursos',
-    'pessoas',
     'usuarios',
     'django.contrib.admin',
     'django.contrib.auth',
--- cursos/migrations/0002_curso_pessoa.py
+++ cursos/migrations/0002_curso_pessoa.py
@@ -2,12 +2,13 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pessoas', '0001_initial'),
+        
         ('cursos', '0001_initial'),
     ]
 
@@ -15,7 +16,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='curso',
             name='pessoa',
-            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, to='pessoas.pessoa'),
-            preserve_default=False,
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
