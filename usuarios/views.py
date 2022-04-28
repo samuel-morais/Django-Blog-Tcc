7ceca744fbdc4a0a4c8cc6c81ee725e6976b2dd2
Samuel Morais <samuelcarlosbsb@hotmail.com>
2022-04-28 08:01:54 -0300

    implementando curso template usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from cursos.models import Curso
 
 def cadastro(request):
     if request.method == 'POST':
@@ -55,20 +56,32 @@ def logout(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render (request,'usuarios/dashboard.html')
+        id = request.user.id
+        cursos = Curso.objects.order_by('-data_curso').filter(pessoa=id)
+       
+
+        dados = { 
+
+            'cursos' : cursos
+        }
+        return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
-#    return render(request,'usuarios/dashboard.html')  
+        
 
 def cria_curso(request):
     if request.method == 'POST':
         nome_curso = request.POST['nome_curso']
-        ementa_curso = request.POST['ementa_curso']
+        ementa_do_curso = request.POST['ementa_do_curso']
         informacoes = request.POST['informacoes']
         carga_horaria = request.POST['carga_horaria']
         categoria = request.POST['categoria']
         foto_curso = request.FILES['foto_curso']
-        print(nome_curso,ementa_curso,informacoes,carga_horaria,categoria,foto_curso)
+        #pegando as informações do usuário
+        user = get_object_or_404(User, pk=request.user.id)
+        #criando objeto de receita
+        curso = Curso.objects.create(pessoa=user,nome_curso = nome_curso, ementa_do_curso = ementa_do_curso, informacoes = informacoes, carga_horaria = carga_horaria,categoria = categoria,foto_curso = foto_curso)
+        curso.save()
         return redirect('dashboard')
     else:
        return render (request,'usuarios/cria_curso.html')
